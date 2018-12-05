cf377d731af6e52d827cc1c0271e0bc0d3b43443
jlin03 <jlin03@stuy.edu>
2018-12-04 19:22:09 -0500

    bug fixes made to selectionsort

--- Sorts.java
+++ Sorts.java
@@ -1,17 +1,17 @@
 public class Sorts {
 	public static void selectionsort(int[] ary) {
 		for(int x = 0; x < ary.length;x++) {
-			int min = 0;
-			for(int i = 0; i < ary.length;i++) {
+			int min = x;
+			for(int i = x; i < ary.length;i++) {
 				if(ary[i] < ary[min]) {
 					min = i;
 				}
 			}
 			int val = ary[min];
-			for(int i = min;i >= 0;i++) {
+			for(int i = min;i > x;i--) {
 				ary[i] = ary[i-1];
 			}
-			ary[0] = val;
+			ary[x] = val;
 		}
 	}
 	
