a92c9c3ccc49f62090f96e77f6a7622ff089beb8
jlin03 <jlin03@stuy.edu>
2018-12-04 19:11:40 -0500

    code block to shift the array when min is found added

--- Sorts.java
+++ Sorts.java
@@ -1,12 +1,16 @@
 public class Sorts {
 	public static void selectionsort(int[] ary) {
 		int min = 0;
-		min = ary[0];
 		for(int i = 0; i < ary.length;i++) {
-			if(ary[i] < min) {
-				min = ary[i];
+			if(ary[i] < ary[min]) {
+				min = i;
 			}
 		}
+		for(int i = min;i >= 0;i++) {
+			ary[i] = ary[i-1];
+		}
+		
+		
 	}
 	
 	
