b00c37f72fa4659252966cda8b9a531768e18efd
jlin03 <jlin03@stuy.edu>
2018-12-04 19:13:58 -0500

    sortselection operation looped for the whole array

--- Sorts.java
+++ Sorts.java
@@ -1,16 +1,18 @@
 public class Sorts {
 	public static void selectionsort(int[] ary) {
-		int min = 0;
-		for(int i = 0; i < ary.length;i++) {
-			if(ary[i] < ary[min]) {
-				min = i;
+		for(int x = 0; x < ary.length;x++) {
+			int min = 0;
+			for(int i = 0; i < ary.length;i++) {
+				if(ary[i] < ary[min]) {
+					min = i;
+				}
 			}
+			int val = ary[min];
+			for(int i = min;i >= 0;i++) {
+				ary[i] = ary[i-1];
+			}
+			ary[0] = val;
 		}
-		int val = ary[min];
-		for(int i = min;i >= 0;i++) {
-			ary[i] = ary[i-1];
-		}
-		ary[0] = val;
 	}
 	
 	
