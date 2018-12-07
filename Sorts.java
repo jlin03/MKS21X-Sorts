af8d8f8b95d847096da0fd18205aae04b0cfb1a1
jlin03 <jlin03@stuy.edu>
2018-12-07 09:59:29 -0500

    bubbleSort coded and tested

--- Sorts.java
+++ Sorts.java
@@ -1,16 +1,38 @@
 public class Sorts {
-	public static void selectionSort(int[] ary) {
-		if(ary.length > 0) {
-			for(int x = 0; x < ary.length;x++) {
-				int min = x;
-				for(int i = x; i < ary.length;i++) {
-					if(ary[i] < ary[min]) {
+	/**Selection sort of an int array.
+  *Upon completion, the elements of the array will be in increasing order.
+  *@param data  the elements to be sorted.
+  */
+	public static void selectionSort(int[] data) {
+		if(data.length > 0) {
+			for(int l = 0; l < data.length;l++) {
+				int min = l;
+				for(int i = l; i < data.length;i++) {
+					if(data[i] < data[min]) {
 						min = i;
 					}
 				}
-				int val = ary[min];
-				ary[min] = ary[x];
-				ary[x] = val;
+				int val = data[min];
+				data[min] = data[l];
+				data[l] = val;
+			}
+		}
+	}
+
+	/**Bubble sort of an int array.
+  *Upon completion, the elements of the array will be in increasing order.
+  *@param data  the elements to be sorted.
+  */
+  public static void bubbleSort(int[] data){
+		if(data.length > 0) {
+			for(int l = 0; l < data.length;l++) {
+				for(int i = 0; i < data.length - 2 - l;i++) {
+					if(data[i] > data[i+1]) {
+						int temp = data[i];
+						data[i] = data[i+1];
+						data[i+1] = temp;
+					}
+				}
 			}
 		}
 	}
