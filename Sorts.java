a1d4a237d527f76d740c97fc493c8d9e2a981bcc
jlin03 <jlin03@stuy.edu>
2018-12-07 09:47:08 -0500

    added check for array length 0 on selectionsort

--- Sorts.java
+++ Sorts.java
@@ -1,15 +1,17 @@
 public class Sorts {
 	public static void selectionSort(int[] ary) {
-		for(int x = 0; x < ary.length;x++) {
-			int min = x;
-			for(int i = x; i < ary.length;i++) {
-				if(ary[i] < ary[min]) {
-					min = i;
+		if(ary.length > 0) {
+			for(int x = 0; x < ary.length;x++) {
+				int min = x;
+				for(int i = x; i < ary.length;i++) {
+					if(ary[i] < ary[min]) {
+						min = i;
+					}
 				}
+				int val = ary[min];
+				ary[min] = ary[x];
+				ary[x] = val;
 			}
-			int val = ary[min];
-			ary[min] = ary[x];
-			ary[x] = val;
 		}
 	}
 
